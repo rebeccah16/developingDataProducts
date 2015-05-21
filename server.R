098caaac537fbc927947f63cc1e67555176ed35a
rebeccah16 <rebeccah16@users.noreply.github.com>
2015-05-21 11:59:01 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,9 +1,16 @@
 library(shiny)
-lowerbound <- function(age) (age / 2)+7 
-upperbound <- function(age) (age-7)*2
+lowerbound <- function(age) round((age / 2)+7)
+upperbound <- function(age) round((age-7)*2)
 
 shinyServer(
   function(input, output) {
+    x <- 5:100
+      y <-(x/2)+7
+    y2 <-(x-7)*2
+    output$distPlot <- renderPlot({
+      plot(x,y,main = 'Change in lower bound age over time',type = 'l',xlab = 'My Age', ylab='Minimum acceptible dating age') })
+ output$distPlot2 <- renderPlot({
+      plot(x,y2,main='Change in upper bound age over time',type = 'l',xlab= 'My Age',ylab='Maximum acceptible dating age') })
     output$inputValue <- renderPrint({input$myage})
     output$lowerprediction <- renderPrint({lowerbound(input$myage)})
     output$upperprediction <- renderPrint({upperbound(input$myage)})
