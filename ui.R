45afd195465f68cf164aafcb595517283f63d1d7
rebeccah16 <rebeccah16@users.noreply.github.com>
2015-05-21 11:59:38 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -4,19 +4,24 @@ library(shiny)
 shinyUI(
   pageWithSidebar(
     # Application title
-    headerPanel("Optimal age for a dating partner or spouse"),
-    
+    headerPanel("Optimal Dating Age Calculator"),
     sidebarPanel(
-      numericInput('myage', 'Age(years)', 90, min = 50, max = 200, step = 5),
-      submitButton('Submit')
+      helpText("This calculator allows you to enter your age and then calculates the minimum and maximum socially-acceptable age for a dating or marriage partner."),
+      h3("Enter your age below to calculate your ideal age range:"),
+      
+      numericInput('myage', 'Age(years)', 25, min = 0, max = 150, step = 1),
+      submitButton('Calculate')
     ),
     mainPanel(
-      h3('Results of prediction'),
-      h4('You entered'),
+      h3('Your results:'),
+      h4('Your current age is:'),
       verbatimTextOutput("inputValue"),
-      h4('Which resulted in a prediction of '),
+      h4('The minimal socially-acceptible dating age for you is:'),
       verbatimTextOutput("lowerprediction"),
-      h4('Test'),
-      verbatimTextOutput("upperprediction")
-    )
-  )
+      h4('The maximum sociall-acceptible dating age for you is:'),
+      verbatimTextOutput("upperprediction"),
+      
+      plotOutput("distPlot"),
+      
+      plotOutput("distPlot2"))
+    ))
